763ca8501859a0aa71a46f4cc2d1c2d2ed10a441
anushakharvi7 <anushakharvi015@gmail.com>
2024-10-23 12:20:31 +0530

    Add files via upload

---
+++ 6.c
@@ -0,0 +1,81 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+#define size 5
+char cq[size],ele;
+int front=-1,rear=-1,ch;
+void enque();
+void deque();
+void display();
+void main()
+{
+	printf("1.Insert 2.Delete 3.Display 4.Exit");
+	while(1)
+	{
+		printf("\n enter your choice:");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:enque();
+				break;
+			case 2:deque();
+				break;
+			case 3:display();
+				break;
+			case 4:exit(0);
+				break;
+		}
+	}
+}
+void enque()
+{
+	if(front==(rear+1)%size)
+	{
+		printf("circular queue overflow");
+		return;
+	}
+	if(front==-1)
+	front++;
+	printf("enter the character to the circular queue");
+	scanf("\n%c",&ele);
+	rear=(rear+1)%size;
+	cq[rear]=ele;
+}
+void deque()
+{
+	char item;
+	if(front==-1)
+	{
+		printf("circular queue underflow");
+		return;
+	}
+	else if(front==rear)
+	{
+		item=cq[front];
+		printf("deleted element is:%c",item);
+		front=-1;
+		rear=-1;
+	}
+	else
+	{
+		item=cq[front];
+		printf("deleted element is:%c",item);
+		front=(front+1)%size;
+	}
+}
+void display()
+{
+	int i;
+	if(front==-1)
+	printf("circular queue is empty");
+	else
+	{
+		printf("circular queue contents are...");
+		for(i=front;i!=rear;i=(i+1)%size)
+		{
+			printf("%c\t",cq[i]);
+		}
+		printf("%c",cq[rear]);		
+	}
+}
+
